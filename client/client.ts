c4d24202ec17090bb9e0644a9142a465d4804a0d
penabrunoj-sys <penabrunoj@gmail.com>
2026-03-08 23:58:51 -0600

    Update client.ts

--- client/client.ts
+++ client/client.ts
@@ -1,4 +1,4 @@
 // Client
-console.log("My address:", pg.wallet.publicKey.toString());
+console.log("Library user address:", pg.wallet.publicKey.toString());
 const balance = await pg.connection.getBalance(pg.wallet.publicKey);
-console.log(`My balance: ${balance / web3.LAMPORTS_PER_SOL} SOL`);
+console.log(`User balance for buying books: ${balance / web3.LAMPORTS_PER_SOL} SOL`);
